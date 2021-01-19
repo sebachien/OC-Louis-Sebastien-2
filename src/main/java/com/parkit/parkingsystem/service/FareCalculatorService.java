e44871d733ec9fbb3fecbf217b9c2152e2deba21
@sebachien <sebastien.louis.69@gmail.com>
2021-01-19 18:53:44 +0100

    Corect TODE#1 calculates time

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -9,12 +9,21 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
+        
+        int inDate = ticket.getInTime().getDate();
         int inHour = ticket.getInTime().getHours();
+        int inMinute = ticket.getInTime().getMinutes();
+        int inSecond = ticket.getInTime().getSeconds();
+
+        int outDate = ticket.getOutTime().getDate();
         int outHour = ticket.getOutTime().getHours();
+        int outMinute = ticket.getOutTime().getMinutes();
+        int outSecond = ticket.getOutTime().getSeconds();
+
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        int duration = (outDate - inDate)*24 + (outHour - inHour) + (((outMinute - inMinute)+((outSecond - inSecond)/60))/60);
+        System.out.println("You have been parked : " + duration + "Hours");
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
