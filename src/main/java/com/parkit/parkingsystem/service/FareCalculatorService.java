43b691f07e6f43f1ea8073709b281a7bcb1a89b4
@sebachien <sebastien.louis.69@gmail.com>
2021-01-20 21:37:10 +0100

    Fix TODE#1 calculates time (v3)

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,8 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -9,29 +12,21 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
+              
+        double diff = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+        double diffHours = (((diff / 1000 )/ 60 )/ 60);
         
-		double inDate = ticket.getInTime().getDate();
-		double inHour = ticket.getInTime().getHours();
-		double inMinute = ticket.getInTime().getMinutes();
-		double inSecond = ticket.getInTime().getSeconds();
-
-		double outDate = ticket.getOutTime().getDate();
-		double outHour = ticket.getOutTime().getHours();
-        double outMinute = ticket.getOutTime().getMinutes();
-        double outSecond = ticket.getOutTime().getSeconds();
-
-
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        double duration = (outDate - inDate)*24 + (outHour - inHour) + (((outMinute - inMinute)+((outSecond - inSecond)/60))/60);
-        System.out.println("You have been parked : " + duration + " Hours");
 
+        System.out.println("You have been parked : " + diffHours + " Hours");
+        
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(diffHours * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(diffHours * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
