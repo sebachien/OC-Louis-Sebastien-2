6727f89f8b8b7dfd176eedcf236a74765f597876
@sebachien <sebastien.louis.69@gmail.com>
2021-01-20 20:06:47 +0100

    Fix TODE#1 calculates time (v2)

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,20 +10,20 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
         
-        int inDate = ticket.getInTime().getDate();
-        int inHour = ticket.getInTime().getHours();
-        int inMinute = ticket.getInTime().getMinutes();
-        int inSecond = ticket.getInTime().getSeconds();
+		double inDate = ticket.getInTime().getDate();
+		double inHour = ticket.getInTime().getHours();
+		double inMinute = ticket.getInTime().getMinutes();
+		double inSecond = ticket.getInTime().getSeconds();
 
-        int outDate = ticket.getOutTime().getDate();
-        int outHour = ticket.getOutTime().getHours();
-        int outMinute = ticket.getOutTime().getMinutes();
-        int outSecond = ticket.getOutTime().getSeconds();
+		double outDate = ticket.getOutTime().getDate();
+		double outHour = ticket.getOutTime().getHours();
+        double outMinute = ticket.getOutTime().getMinutes();
+        double outSecond = ticket.getOutTime().getSeconds();
 
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = (outDate - inDate)*24 + (outHour - inHour) + (((outMinute - inMinute)+((outSecond - inSecond)/60))/60);
-        System.out.println("You have been parked : " + duration + "Hours");
+        double duration = (outDate - inDate)*24 + (outHour - inHour) + (((outMinute - inMinute)+((outSecond - inSecond)/60))/60);
+        System.out.println("You have been parked : " + duration + " Hours");
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
