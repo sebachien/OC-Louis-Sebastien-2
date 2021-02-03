8b920b595a1588312a1c4c2eb74ce20c0439338a
@sebachien <sebastien.louis.69@gmail.com>
2021-02-03 18:02:46 +0100

    Fix TODE#2 and and 30-min free and 5%-discount (+test) (v2)

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -82,12 +82,7 @@ public class TicketDAO {
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
                 ticket = new Ticket();
-                //ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),false);
-                //ticket.setParkingSpot(parkingSpot);
-                //ticket.setId(rs.getInt(2));
                 ticket.setVehicleRegNumber(vehicleRegNumber);
-                //ticket.setPrice(rs.getDouble(3));
-                //ticket.setInTime(rs.getTimestamp(4));
                 ticket.setOutTime(rs.getTimestamp(5));
             }
             dataBaseConfig.closeResultSet(rs);
